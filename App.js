7002a67803097452e574307d8ac292813fc846c3
AdityaDeshmukh1080 <122623811+AdityaDeshmukh1080@users.noreply.github.com>
2023-06-30 23:51:12 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Contact from './Project4/Contact'
+function App() {
+  return (
+    <div>
+    
+      <Contact/>
+      
+    </div>
+  )
+}
+
+export default App
